4579883b149403923c3295d9a5b68722a24dce54
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-02-09 12:06:53 -0300

    Add section container

--- src/components/DocSection/DocSection.tsx
+++ src/components/DocSection/DocSection.tsx
@@ -8,6 +8,17 @@ const DocSection = () => {
                 <img src={Beginner} alt="Beginner icon" className="mr-3" />
                 <Title title="Guia para iniciar en Accesibilidad Web" />
             </div>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                <div className="bg-white">
+                    A
+                </div>
+                <div className="row-span-2 bg-white">
+                    B
+                </div>
+                <div className="bg-white">
+                    C
+                </div>
+            </div>
         </section>
     )
 }
