af9c7a3e66b4c057ccecc2675461b50a29c0067d
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-02-28 19:29:26 -0300

    Add cards selected

--- src/components/AnalyzePage/PageResults.tsx
+++ src/components/AnalyzePage/PageResults.tsx
@@ -1,60 +1,99 @@
-import { memo, useMemo } from "react";
+import { memo, useMemo, useState } from "react";
 import { Pa11y } from "../../utils/api";
+import IssueResumeCard from "./IssueResumeCard";
 
 interface PageResultsProps {
   pa11yResults: Pa11y;
 }
 
-const codeTypeColor: any = {
-  error: "red",
-  warning: "#EED202",
-  notice: "#000080",
-};
+enum ResumeCards {
+  Error = 1,
+  Warning = 2,
+  Notice = 3,
+}
 
 // eslint-disable-next-line react-refresh/only-export-components
 function PageResults({ pa11yResults }: PageResultsProps) {
+  const [cardsSelected, setCardsSelected] = useState([
+    ResumeCards.Error,
+    ResumeCards.Warning,
+    ResumeCards.Notice,
+  ]);
+
+  const onClickCard = (id: number) => {
+    if (cardsSelected.includes(id)) {
+      setCardsSelected((prevState) =>
+        prevState.filter((selectedCard) => selectedCard !== id)
+      );
+    } else {
+      setCardsSelected((prevState) => [...prevState, id]);
+    }
+  };
   const { issueCountByType, data } = pa11yResults;
   const { error, warning, notice } = issueCountByType;
-  const issuesResume = useMemo(() => {
-    return [
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const colors: any = useMemo(
+    () => ({
+      error: {
+        background: "bg-red-500",
+        hover: "hover:bg-red-400",
+        border: "border-red-500",
+        selected: cardsSelected.includes(ResumeCards.Error),
+      },
+      warning: {
+        background: "bg-yellow-500",
+        hover: "hover:bg-yellow-400",
+        border: "border-yellow-500",
+        selected: cardsSelected.includes(ResumeCards.Warning),
+      },
+      notice: {
+        background: "bg-blue-800",
+        hover: "hover:bg-blue-700",
+        border: "border-blue-800",
+        selected: cardsSelected.includes(ResumeCards.Notice),
+      },
+    }),
+    [cardsSelected]
+  );
+
+  const cards = useMemo(
+    () => [
       {
+        id: ResumeCards.Error,
         label: "ERRORES",
         amount: error,
-        color: codeTypeColor.error,
+        color: colors.error,
       },
       {
+        id: ResumeCards.Warning,
         label: "ADVERTENCIAS",
         amount: warning,
-        color: codeTypeColor.warning,
+        color: colors.warning,
       },
       {
-        label: "OBSERVACIONES",
+        id: ResumeCards.Notice,
+        label: "AVISOS",
         amount: notice,
-        color: codeTypeColor.notice,
+        color: colors.notice,
       },
-    ];
-  }, [error, warning, notice]);
+    ],
+    [colors, error, warning, notice]
+  );
 
   return (
     <div className="max-w-3xl m-auto mt-10 p-8 lg:p-0">
       <div className="w-full grid grid-cols-1 gap-3 sm:grid-cols-3">
-        {issuesResume.map(({ label, amount, color }, index) => (
-          <div
-            key={index}
-            className="border-2 w-full rounded-md p-3"
-            style={{ borderColor: color }}
-          >
-            <h3 className="font-medium text-xl text-gray-700">{label}</h3>
-            <h4 className="font-medium text-6xl">{amount}</h4>
-          </div>
+        {cards.map((props) => (
+          <IssueResumeCard {...props} onClickCard={onClickCard} />
         ))}
       </div>
       <div className="w-full flex flex-col mt-4 ">
         {data.issues.map((issue, index) => (
           <div
             key={index}
-            className="border-2 mb-4 p-4 bg-white text-start"
-            style={{ borderColor: codeTypeColor[issue.type] }}
+            className={`border-2 rounded-md mb-4 p-4 bg-white text-start`}
+            style={{ borderColor: colors[issue.type]?.border }}
           >
             <h2 className="text-lg mb-2">
               <span className="uppercase font-bold">{issue.type}</span>:{" "}
@@ -73,4 +112,5 @@ function PageResults({ pa11yResults }: PageResultsProps) {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default memo(PageResults);
